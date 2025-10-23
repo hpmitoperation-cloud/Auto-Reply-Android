f6a2f86f02a1fbcb5b7a68822ed5ee73f7375791
hpmitoperation-cloud <hpmitoperation@gmail.com>
2025-10-24 00:33:58 +0700

    Update build.gradle.kts
    git add build.gradle.kts
    git commit -m "Fix: migrate root build.gradle to Gradle 8+ DSL"
    git push

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.6.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.20")
-        classpath("com.google.gms:google-services:4.4.2")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:3.0.2")
-    }
+plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.library") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
 }
 
 allprojects {
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
-}
+// Optional: disable Gradle caching if CI errors occur
+gradle.startParameter.isBuildCacheEnabled = false
